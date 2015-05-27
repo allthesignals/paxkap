ef8f9b0ed068929fa030195a20bc72cbf5c300f9
allthesignals <wilburnforce@gmail.com>
2015-05-26 20:25:25 -0400

    New models, new layout.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,9 +17,8 @@
 //= require_tree .
 (function($){
   $(function(){
-
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-
+    $('.slider').slider({full_width: true});
   }); // end of document ready
 })(jQuery); // end of jQuery name space
